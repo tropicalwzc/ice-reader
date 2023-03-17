ae0ba895b72891a298ac5fa21bb7976e9cb73e86
Tropical fish <wzc990503@gmail.com>
2023-03-18 07:29:05 +0800

    Initial Commit

---
+++ ice reader/ice_readerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ice_readerApp.swift
+//  ice reader
+//
+//  Created by 王子诚 on 2023/3/18.
+//
+
+import SwiftUI
+
+@main
+struct ice_readerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
